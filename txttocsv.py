3d779f65242b61d0ea4efc99d649dda343e9161f
Vlad <durdeuvladioan@gmail.com>
2023-04-14 22:16:11 +0300

    Fixed bugs regarding having multiple words in answers

--- txttocsv.py
+++ txttocsv.py
@@ -19,17 +19,16 @@ while i < len(lines):
     # Check if this is a question line
     if lines[i].find('.'):
         question = lines[i].strip()
+        print("Question detected: "+ question)
         answers = []
         points = []
         # Loop through the next 5 lines to get the answers and points
-        for j in range(i+1, i+MAX_ANSWERS-1):
+        for j in range(i+1, i+MAX_ANSWERS):
             print("SUNTEM cu j PE LINIA", j)
                
             try:
-                answer, point = lines[j].strip().split()
-                answer = answer.replace('—', '')
-                answer = answer.replace('â€”', '')
-                answer = answer.replace('\"\"', '')
+                line = lines[j].strip().replace('—', '').replace('â€”', '').replace('\"\"', '')
+                answer, point = line.rsplit(' ', 1)
                 print(". ANSWER: "+ answer+ " POINT: "+point)
                 
                 answers.append(answer)
@@ -47,8 +46,18 @@ while i < len(lines):
 
             # Remove the dashes from the answer
         # Add the data to the output list
+
+        ok = 1
         row = [question] + answers
-        if row[0]!='':
+        if i!=0:
+            row.pop(0)
+            try:
+                row[1] = row[0]+" "+row[1]
+                row.pop(0)
+            except IndexError:
+                print("Am gasit IndexError")
+                ok=0
+        if row!='' and ok == 1:
             print("Bagam linia: ", row)
             output_data.append(row)
         
